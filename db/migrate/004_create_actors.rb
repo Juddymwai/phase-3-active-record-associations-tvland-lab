2b02e312b3e92f61c7ef70d600df7b6467edf5c7
Judy Mwai <juddiemukami@gmail.com>
2022-09-02 22:09:15 +0300

    done

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,12 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
+
     # Your code here
+    create_table :actors do |t|
+      
+      t.string :first_name
+      t.string :last_name
+    end
+    # add_column :name, text:actor_id, :integer :show_id, :integer
   end
 end
